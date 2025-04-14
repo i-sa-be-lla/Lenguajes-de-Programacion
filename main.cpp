747159bb5f25117cfac5a11779ad8d6f22d6e437
i-sa-be-lla <girl_isa_bella@hotmail.com>
2025-04-13 21:44:12 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    int edad;
+    std::cout << "ingrese su edad: ";
+    std::cin >> edad;
+    
+    if (edad >= 18) {
+        std::cout << "usted es mayor de edad." << std::endl;
+    } else {
+        std::cout << "usted es menor de edad." << std::endl;
+    }
+    return 0;
+}
